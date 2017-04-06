1dff03a30cccc840a4f77d1333507d407555c0a6
Pavel Polyakov <pavel@kredito.de>
2017-04-06 15:12:41 +0200

    clean version

--- conf/webpack.dev.config.js
+++
@@ -1,4 +0,0 @@
-const webpack = require('webpack');
-const wc = require('webpack-config');
-
-module.exports = new wc.Config().extend('conf/webpack.base.config.js').merge({});
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,75 @@
 const webpack = require('webpack');
-const wc = require('webpack-config');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const _ = require('lodash');
 
-wc.environment.setAll({
-    env: () => process.env.NODE_ENV ? process.env.NODE_ENV : 'dev'
-});
-
-// Also you may use `'conf/webpack.[NODE_ENV].config.js'`
-module.exports = new wc.Config().extend('conf/webpack.[env].config.js');
+module.exports = {
+    entry: {
+        app: ['./app/index.js']
+    },
+    output: {
+        filename: '[name].bundle.js',
+        sourceMapFilename: '[name].bundle.map',
+        path: path.resolve(__dirname, './dist')
+    },
+    devServer: {
+        hot: true,
+        port: 3001,
+        contentBase: path.resolve(__dirname, './dist'),
+        historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'babel-loader',
+                        options: {
+                            presets: [['es2015', { 'modules': false }], 'react'],
+                            plugins: ['transform-async-to-generator',
+                                'transform-decorators-legacy',
+                                'transform-runtime',
+                                'react-html-attrs',
+                                'react-hot-loader/babel'],
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.css/,
+                use: ['style-loader', 'css-loader', 'postcss-loader']
+            },
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|gif|jpg|png)$/,
+                use: 'file-loader'
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './index.html',
+            inject: "body"
+        }),
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'lodash+moment',
+            minChunks: function (module) {
+                let result = module.context && _.reduce(['lodash', 'moment'], (result, record) => result || _(module.context).includes(`node_modules/${record}`), false);
+                return result;
+            }
+        }),
+        // put everything else in the other file
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            minChunks: function (module) {
+                let result = module.context && _(module.context).includes(`node_modules`) && !_.reduce(['lodash', 'moment'], (result, record) => result || _(module.context).includes(`node_modules/${record}`), false);
+                return result;
+            }
+        })
+    ],
+    resolve: {
+        extensions: ['.js', '.jsx']
+    },
+    devtool: 'source-map'
+}
